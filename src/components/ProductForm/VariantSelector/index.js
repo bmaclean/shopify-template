23b549deefc9d71da51634407cacacd2c150d4fd
Brendan M <brendan.maclean94@gmail.com>
2019-08-10 14:26:48 -0600

    Linting existing files. Removes some unnessecary files.

--- src/components/ProductForm/VariantSelector/index.js
+++ src/components/ProductForm/VariantSelector/index.js
@@ -1,37 +1,33 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const VariantSelector = props => {
-  const { option } = props
-  return (
-    <>
-      <label htmlFor={option.name}>{option.name} </label>
-      <select
-        name={option.name}
-        key={option.id}
-        onChange={props.onChange}
-      >
-        {option.values.map(value => {
-          return (
-            <option
-              value={value}
-              key={`${option.name}-${value}`}
-            >{`${value}`}</option>
-          )
-        })}
-      </select>
-      <br/>
-    </>
-  )
-}
+	const {option} = props;
+	return (
+		<>
+			<label htmlFor={option.name}>{option.name} </label>
+			<select name={option.name} key={option.id} onChange={props.onChange}>
+				{option.values.map(value => {
+					return (
+						<option
+							value={value}
+							key={`${option.name}-${value}`}
+						>{`${value}`}</option>
+					);
+				})}
+			</select>
+			<br />
+		</>
+	);
+};
 
 VariantSelector.propTypes = {
-  onChange: PropTypes.func,
-  option: PropTypes.shape({
-    id: PropTypes.string,
-    name: PropTypes.string,
-    values: PropTypes.arrayOf(PropTypes.string),
-  }),
-}
+	onChange: PropTypes.func,
+	option: PropTypes.shape({
+		id: PropTypes.string,
+		name: PropTypes.string,
+		values: PropTypes.arrayOf(PropTypes.string)
+	})
+};
 
-export default VariantSelector
+export default VariantSelector;
