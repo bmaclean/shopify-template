e65c2c2fa06bea6ea6814941103ffcb7c7dda902
Brendan M <brendan.maclean94@gmail.com>
2019-08-10 15:46:51 -0600

    Remove margins on page content.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -136,14 +136,7 @@ class Layout extends React.Component {
 					render={data => (
 						<>
 							<Header siteTitle={data.site.siteMetadata.title} />
-							<div
-								style={{
-									margin: '0 auto',
-									maxWidth: 960,
-									padding: '0px 1.0875rem 1.45rem',
-									paddingTop: 0
-								}}
-							>
+							<div>
 								{children}
 								<footer>
 									© {new Date().getFullYear()}, Built with{' '}
