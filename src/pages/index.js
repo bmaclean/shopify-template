40c4be47f284f8613ea32700296eb0e1a4ad16f4
Brendan M <brendan.maclean94@gmail.com>
2019-08-10 15:46:18 -0600

    Add background image and text to index page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,40 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from 'react';
+import {Link} from 'gatsby';
 
-import SEO from '../components/seo'
-import ProductGrid from '../components/ProductGrid'
+import SEO from '../components/seo';
+import ProductGrid from '../components/ProductGrid';
+import BackgroundImage from '../images/xbox-background.png';
 
-const IndexPage = () => (
-  <>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Shop powered by Gatsby and Shopify.</p>
-    <ProductGrid />
-    <Link to="/page-2/">Go to page 2</Link>
-  </>
-)
+const IndexPage = ({image}) => (
+	<>
+		<SEO title="Home" keywords={['gatsby', 'application', 'react']} />
+		<div
+			css={{
+				backgroundImage: `linear-gradient(#101010, transparent 200%), url(${BackgroundImage})`,
+				backgroundPosition: 'top left',
+				backgroundAttachment: 'fixed',
+				backgroundRepeat: 'no-repeat',
+				backgroundSize: 'cover',
+				height: '800px',
+				width: '100%',
+				'-webkit-filter': 'grayscale(100%)'
+			}}
+		>
+			<div
+				css={{
+					display: 'flex',
+					height: '100%',
+					lineHeight: '1',
+					justifyContent: 'center',
+					alignItems: 'center',
+					flexDirection: 'column'
+				}}
+			>
+				<h1 css={{color: 'white'}}>Your Console. Your Style.</h1>
+			</div>
+		</div>
+		<ProductGrid />
+	</>
+);
 
-export default IndexPage
+export default IndexPage;
