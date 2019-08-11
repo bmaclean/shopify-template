34629d410b6fc1e76eaf02eb94f43311d9f51e5e
brendanmaclean <brendan.maclean@alumni.ubc.ca>
2019-08-11 00:55:26 -0600

    Add Call to Action and About blurb on landing page.

---
+++ src/components/CTAButton.js
@@ -0,0 +1,21 @@
+import {Link} from 'gatsby';
+import styled from '@emotion/styled';
+
+// Call To Action button
+
+const CTAButton = styled(Link)`
+	color: #ffffff;
+	background-color: #d71111;
+	padding: 12px 20px;
+	font-family: Montserrat, sans-serif;
+	font-weight: 600;
+	font-style: normal;
+	letter-spacing: 0.1em;
+	text-transform: uppercase;
+	font-size: 0.8125em;
+	text-align: center;
+	cursor: pointer;
+	text-decoration: none;
+`;
+
+export default CTAButton;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {Link} from 'gatsby';
+import {Flex, Box} from '@rebass/grid/emotion';
 
-import SEO from '../components/seo';
+import CTAButton from '../components/CTAButton';
 import ProductGrid from '../components/ProductGrid';
+import SEO from '../components/seo';
+
 import BackgroundImage from '../images/xbox-background.png';
 
 const IndexPage = ({image}) => (
@@ -15,25 +18,48 @@ const IndexPage = ({image}) => (
 				backgroundAttachment: 'fixed',
 				backgroundRepeat: 'no-repeat',
 				backgroundSize: 'cover',
-				height: '800px',
-				width: '100%',
-				'-webkit-filter': 'grayscale(100%)'
+				height: '100vh',
+				width: '100%'
 			}}
 		>
 			<div
 				css={{
 					display: 'flex',
 					height: '100%',
-					lineHeight: '1',
 					justifyContent: 'center',
 					alignItems: 'center',
-					flexDirection: 'column'
+					flexDirection: 'column',
+					lineHeight: 1.42
 				}}
 			>
-				<h1 css={{color: 'white'}}>Your Console. Your Style.</h1>
+				<h1 css={{color: 'white', fontSize: '3em'}}>
+					Your Console. Your Style.
+				</h1>
+				<CTAButton to="/shop">Shop Console Skins</CTAButton>
 			</div>
 		</div>
-		<ProductGrid />
+		<Flex
+			css={{
+				height: '28em',
+				fontFamily: 'Didot, serif'
+			}}
+			justifyContent="center"
+			alignItems="center"
+			flexDirection="column"
+		>
+			<h1>Why ConsoleStyle?</h1>
+			<p>
+				When everything else in life is customizable, your console should be
+				too.
+			</p>
+
+			<p>
+				It&apos;s time to define your personality, and pick the skin that lets
+				you best express yourself.
+			</p>
+
+			<p>Don&apos;t let your console stay bland. Give it ConsoleStyle.</p>
+		</Flex>
 	</>
 );
 
