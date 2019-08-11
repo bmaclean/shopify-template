2d66f6b8619a0d41f4fbd37b273fb66ac9b6b214
brendanmaclean <brendan.maclean@alumni.ubc.ca>
2019-08-11 00:54:42 -0600

    Minor refactor and Gatsby config updates.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ require('dotenv').config({
 });
 module.exports = {
 	siteMetadata: {
-		title: 'Console Style',
+		title: 'ConsoleStyle',
 		description: 'Your console. Your style.',
 		author: '@bmaclean'
 	},
@@ -28,7 +28,7 @@ module.exports = {
 				background_color: '#663399',
 				theme_color: '#663399',
 				display: 'minimal-ui',
-				icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+				icon: 'src/images/logo.png' // This path is relative to the root of the site.
 			}
 		},
 		{
