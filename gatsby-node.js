2832fe334d320f33b7c31366c059f017d55af039
Brendan M <brendan.maclean94@gmail.com>
2019-08-14 16:50:11 -0600

    Generate collection page stubs

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,12 +10,30 @@ exports.createPages = ({graphql, actions: {createPage}}) => {
 					}
 				}
 			}
+			allShopifyCollection {
+				edges {
+					node {
+						handle
+					}
+				}
+			}
 		}
 	`).then(result => {
 		result.data.allShopifyProduct.edges.forEach(({node}) => {
 			createPage({
 				path: `/product/${node.handle}/`,
 				component: path.resolve('./src/templates/ProductPage/index.js'),
+				context: {
+					// Data passed to context is available
+					// in page queries as GraphQL variables.
+					handle: node.handle
+				}
+			});
+		});
+		result.data.allShopifyCollection.edges.forEach(({node}) => {
+			createPage({
+				path: `/shop/${node.handle}/`,
+				component: path.resolve('./src/templates/CollectionPage/index.js'),
 				context: {
 					// Data passed to context is available
 					// in page queries as GraphQL variables.
