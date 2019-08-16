f20799625d33d739621f9cc79e86252a22864000
Brendan M <brendan.maclean94@gmail.com>
2019-08-15 18:16:56 -0600

    Fix collection links and styling.

--- src/components/NavBox.js
+++ src/components/NavBox.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import {Link} from 'gatsby';
 import {Flex, Box} from '@rebass/grid/emotion';
 import {CenteredFlex} from './ui';
+
+// TODO: subpage links should be generated, not explicitly provided
 
 export default function NavBox({image, subpages}) {
 	const [selected, setSelected] = React.useState(subpages[0]);
@@ -19,31 +22,41 @@ export default function NavBox({image, subpages}) {
 			<Flex css={{height: '80%'}}></Flex>
 			<Flex css={{color: 'white', height: '20%', width: '100%'}}>
 				{subpages.map(subpage => (
-					<CenteredFlex
+					<Link
 						key={subpage.title}
-						flex="1"
-						onMouseOver={() => setSelected(subpage)}
+						to={`/shop/${subpage.link}`}
 						css={{
-							// background:
-							// 	selected.title === subpage.title
-							// 		? 'rgba(255, 255, 255, 0.3)'
-							// 		: '',
-							// TODO: duplicate font styles
-							fontFamily: 'Montserrat, sans-serif',
-							fontWeight: 600,
-							fontStyle: 'normal',
-							letterSpacing: '0.1em',
-							textTransform: 'uppercase',
-							fontSize: '0.8125em',
-							cursor: 'pointer',
-							'&:hover': {
+							flex: '1',
+							color: '#ffffff',
+							textDecoration: 'none',
+							'&.visited': {
 								color: '#111111',
-								background: 'rgba(255, 255, 255, 0.5)'
+								textDecoration: 'none'
 							}
 						}}
 					>
-						{subpage.title}
-					</CenteredFlex>
+						<CenteredFlex
+							flex="1"
+							onMouseOver={() => setSelected(subpage)}
+							css={{
+								fontFamily: 'Montserrat, sans-serif',
+								fontWeight: 600,
+								fontStyle: 'normal',
+								letterSpacing: '0.1em',
+								height: '100%',
+								textTransform: 'uppercase',
+								fontSize: '0.8125em',
+								cursor: 'pointer',
+								textDecoration: 'none',
+								'&:hover': {
+									color: '#111111',
+									background: 'rgba(255, 255, 255, 0.5)'
+								}
+							}}
+						>
+							{subpage.title}
+						</CenteredFlex>
+					</Link>
 				))}
 			</Flex>
 		</div>
